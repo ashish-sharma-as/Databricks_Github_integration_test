eff29ec57e070f2a857d2a984be103297d5d3f43
ashish.sharma.as@gmail.com <ashish.sharma.as@gmail.com>
2021-07-02 15:07:24 +0000

    Testing Databricks and Github integration
    Working on project that requires Databricks and github integration. Testing this with a test repo

---
+++ databricks_notebk_test.py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+# Test Databricks-Gitbhub integration
+# This is just a test databricks notebook and can be deleted by anyone.
+
+
+
+# COMMAND ----------
+
+import pandas as pd
+
+df = pd.DataFrame({'col1':[1,2,3], 'col2':[4,5,6]})
+df
+
+# COMMAND ----------
+
+print(df.shape)
+
+# COMMAND ----------
+
+# Done.
+
+# COMMAND ----------
+
+
