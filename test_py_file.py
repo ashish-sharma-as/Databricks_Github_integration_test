fc019e7de0b9bfc99cee203860440ec2d48226b9
ashish.sharma.as@gmail.com <ashish.sharma.as@gmail.com>
2021-07-03 18:33:19 +0000

    Merge conflict commit

--- test_py_file.py
+++ test_py_file.py
@@ -1,9 +1,4 @@
+# Databricks notebook source
 """
-Dummy file on dummy branch to test github-db integration
 """
-# Testing py file DB
-# Another comment
 print('Testing .py file')
-# Last comment
-# Yet another
-# Ok 1 more. something is not right
